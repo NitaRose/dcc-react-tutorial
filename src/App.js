44bf096d02539d6c9489079ed35e35f6c6bbedc7
Nita Rose <rose_nita@outllok.com>
2023-08-31 22:09:53 -0400

    Created component and Instantiated component in app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import React, { useState } from 'react';
-
-
-
+import React, { useState } from "react";
+import DisplayEntries from "./Componets/DisplayEntries/DisplayEntries";
 
 function App() {
-
-
-const [ entries, setEntries] = useState([{weight: 175, date: '11.22.2023'}])
-  return (
-
-    <div> 
-     <table>
-      <thead>
-        <tr>
-          <th>Entry Number</th>
-          <th>Weight</th>
-          <th>Date</th>
-        </tr>
-      </thead>
-     </table>
-    </div>
-  );
+  const [entries, setEntries] = useState([
+    { weight: 175, date: "11.22.2023" },
+    { weight: 176, date: "11.23.2023" },
+  ]);
+  return <div></div>;
 }
 
 export default App;
