7385df7ec96074ce7139bfe74dde288ac1c7a10b
Nita Rose <rose_nita@outllok.com>
2023-09-01 12:36:20 -0400

    Created onCHange in AddEntryForm.jsx

--- src/Componets/AddEntry/AddEntryForm.jsx
+++ src/Componets/AddEntry/AddEntryForm.jsx
@@ -3,15 +3,15 @@ import React, { useState } from "react";
 
 const AddEntryForm = (props) => {
 
-    const[weight, setWeight] = useState[0]
-    const[date, setDate] = useState[""];
+    const [weight, setWeight] = useState(0);
+    const [date, setDate] = useState("");
 
   return (
     <form>
         <label>Weight</label>
-        <input type="number" value={weight} />
+        <input type="number" value={weight} onChange={(event) => setWeight(event.target.value)} />
         <label>Date</label>
-        <input type="date" value={date} />
+        <input type="date" value={date} onChange={(event) => setDate(event.target.value)}/>
     </form>
   );
 };
